770b3c48f650697c3c69bae2307fe24ae8b61e41
kazimirovi4 <85028320+kazimirovi4@users.noreply.github.com>
2024-09-03 18:34:39 +0300

    Add files via upload

---
+++ drf/tpi_drf/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'), name='logout'),
+    path('chats/', views.user_chat_list, name='chat_list'),
+    path('chat/<uuid:chat_uuid>/', views.chat_detail, name='chat_detail'),
+    path('admin/chats/', views.admin_chat_list, name='admin_chat_list'),
+    path('admin/chat/<uuid:chat_uuid>/', views.admin_chat_detail, name='admin_chat_detail'),
+    path('create_chat/', views.create_chat, name='create_chat'),
+    path('chat/<uuid:chat_uuid>/close/', views.close_chat, name='close_chat'),
+    path('update_status/<str:status>/', views.update_status, name='update_status'),
+]
